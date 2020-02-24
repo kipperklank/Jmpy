e9ea8d07988a48b3fbc82ea9215d62fb34c56e35
kipperklank <delphinusmb@gmail.om>
2020-02-24 01:25:57 -0600

    Test

--- Jmpy_master.py
+++ Jmpy_master.py
@@ -1,6 +1,15 @@
 
 import turtle
-import tkinter
+go = turtle.Turtle()
+turtle.Screen()
+go.speed(0)
+go.color('blue')
+x=0.5
+for i in range(10000):
+   go.forward(x)
+   x=x+0.5
 
-turtle.forward(15)
+   go.left(44)
 
+
+turtle.done()
